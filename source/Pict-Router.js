1ab9c456e86ead06fa8afe12013daf7405108e59
samwatson <sam.watson@headlight.com>
2024-10-16 16:17:13 -0400

    Adding resolve() call after adding a route to navigo so the router will properly interpret routes navigated to directly through url

--- source/Pict-Router.js
+++ source/Pict-Router.js
@@ -1,5 +1,6 @@
 const libPictProvider = require('pict-provider');
 const libNavigo = require('navigo');
+const {ResolveOptions} = require("navigo");
 
 const _DEFAULT_PROVIDER_CONFIGURATION =
 {
@@ -65,6 +66,7 @@ class PictRouter extends libPictProvider
 		if (typeof(pRenderable) === 'function')
 		{
 			this.router.on(pRoute, pRenderable);
+			this.resolve();
 		}
 		else if (typeof(pRenderable) === 'string')
 		{
@@ -74,6 +76,7 @@ class PictRouter extends libPictProvider
 				{
 					this.pict.parseTemplate(pRenderable, pData, null, this.pict)
 				});
+			this.resolve();
 		}
 		else
 		{
@@ -90,6 +93,14 @@ class PictRouter extends libPictProvider
 	navigate(pRoute)
 	{
 		this.router.navigate(pRoute);
+	}
+
+	/**
+	 * Trigger the router resolving logic
+	 *
+	 */
+	resolve() {
+		this.router.resolve();
 	}
 }
 
